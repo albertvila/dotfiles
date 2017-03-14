7a590e1e3390d903085cbb75be90ad87abca9d05
Albert Vila <albert.vila@gmail.com>
2017-03-14 21:09:22 +0000

    Add relative bin folder to path

--- zsh/custom/default.zsh
+++ zsh/custom/default.zsh
@@ -1,7 +1,6 @@
 source ~/.tmuxinator.zsh
 
-export PATH=$PATH:/Users/albepuig/bin
+export PATH=~/bin:$PATH
 
 # Needed when running tmux inside iTerm2 locally
 export TERM="xterm-256color"
-export PATH
