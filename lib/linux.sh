f746ab15d302b0365d6653288c838604bc0800ce
Albert Vila <albert.vila@launchmetrics.com>
2017-09-20 09:31:56 +0200

    Atom installation for linux done

--- lib/linux.sh
+++ lib/linux.sh
@@ -3,6 +3,8 @@
 function install_linux_packages() {
   error "Linux support not implemented yet, pending to install some packages, see lib/osx.sh"
   error "Check if this could be useful http://linuxbrew.sh/ or just use apt commands"
+
+  _install_atom
 }
 
 # # Needed just for linux OS (pending to be tested)
@@ -14,3 +16,29 @@ function install_linux_packages() {
 #   zsh # https://gist.github.com/tsabat/1498393
 #   intellij-idea-community # https://itsfoss.com/install-intellij-ubuntu-linux/
 # )
+
+function _install_atom_packages() {
+  for pkg in ${ATOM_PACKAGES[@]}; do
+    if [[ ! -d "$HOME/.atom/packages/$pkg" ]]
+    then
+      warn "[atom] Package '$pkg' is not installed"
+      apm install $pkg
+    else
+      ok "[atom] Package '$pkg' is already installed"
+    fi
+  done
+  unset ATOM_PACKAGES
+}
+
+function _install_atom() {
+  bot "Checking atom packages ..."
+
+  if ! test $(which atom)
+  then
+    error "atom not installed"
+  else
+    _install_atom_packages
+  fi
+
+  ok
+}
