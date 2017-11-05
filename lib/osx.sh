9df22ac4575cef80ee039c6ed8875fbc052429a0
Albert Vila <albert.vila@gmail.com>
2017-11-05 08:32:17 +0100

    Adding a lot more osx defaults automatically

--- lib/osx.sh
+++ lib/osx.sh
@@ -10,26 +10,49 @@ function install_osx_packages() {
 }
 
 function _setup_osx() {
+  bot "Setting up osx ..."
+
   # show full paths in Finder
   defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
 
+  # Disable and kill Dashboard
+  defaults write com.apple.dashboard mcx-disabled -boolean YES
+
+  # Show all file extensions on Finder
+  defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+  # Disable natural trackpad scrolling (TODO however it seems it does not work)
+  defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+  # Don't automatically rearrange Spaces
+  defaults write com.apple.dock mru-spaces -bool false
+
+  # Set column view as preferred Finder view
+  defaults write com.apple.finder FXPreferredViewStyle -string 'clmv'
+
+  # Search the current folder by default
+  defaults write com.apple.finder FXDefaultSearchScope -string 'SCcf'
+
+  # Remove all applications from Dock
+  defaults write com.apple.dock persistent-apps -array
+
+  # Set bottom right hot corner to show/hide desktop
+  defaults write com.apple.dock wvous-br-corner -int 4
+  defaults write com.apple.dock wvous-br-modifier -int 0
+
+  # Update clock to show current date and current day of the week
+  defaults write com.apple.menuextra.clock DateFormat 'EEE MMM d  h:mm a'
+
+  # Show the ~/Library directory in Finder
+  chflags nohidden "${HOME}/Library"
+
+  # Show finder status bar
+  defaults write com.apple.finder ShowStatusBar -bool true
+
+  killall Dock Finder SystemUIServer
+
   installation_mode=$(get_installation_mode)
   if [ $installation_mode == "install" ]; then
-    # Disable and kill Dashboard
-    defaults write com.apple.dashboard mcx-disabled -boolean YES
-
-    # Show all file extensions on Finder
-    defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-    # Disable natural trackpad scrolling (TODO however it seems it does not work)
-    defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-
-    # Show the ~/Library directory in Finder
-    chflags nohidden "${HOME}/Library"
-    defaults write com.apple.finder ShowStatusBar -bool true
-
-    killall Finder
-    killall Dock
 
     # Install the Solarized Dark theme for iTerm
     open "${DOTFILES_DIR}/iterm/themes/Solarized Dark.itermcolors"
