1fa6b450a9d5f789d126618bdb7244d77249d5ec
Albert Vila <albert.vila@gmail.com>
2018-01-22 16:12:36 +0100

    Shows terraform worskapce on zsh powerline theme

--- zsh/scripts/functions.zsh
+++ zsh/scripts/functions.zsh
@@ -6,6 +6,14 @@ function infinite {
   done
 }
 
+function prompt_terraform() {
+
+  if [[ -n *.tf(#qN) ]]; then
+    WORKSPACE=$(terraform workspace show 2> /dev/null) || return
+    "$1_prompt_segment" "$0" "$2" "$DEFAULT_COLOR" "red" "tf:$WORKSPACE"
+  fi
+}
+
 # an osx substitution for linux 'free' command
 function free {
 
