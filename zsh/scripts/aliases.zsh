b05da46aabde4d7a6f95805fa16d92081f36c7fe
Albert Vila <albert.vila@gmail.com>
2018-12-11 10:34:59 +0100

    New gbranch method to automatically create a random branch from master/develop

--- zsh/scripts/aliases.zsh
+++ zsh/scripts/aliases.zsh
@@ -16,3 +16,26 @@ alias fixSound="sudo killall coreaudiod"
 
 # Git stash only for not added files
 alias gstk="git stash --keep-index"
+
+gbranch() {
+    git fetch
+
+    currentBranch=`git branch | grep \* | cut -d ' ' -f2`
+
+    if test "$currentBranch" = 'develop'; then
+        baseBranch='d'
+    elif test "$currentBranch" = 'master'; then
+        baseBranch='m'
+    else
+        echo "Wrong branch, you should be in master or develop branch"
+        exit 1;
+    fi
+
+    shortCommitHash=`git rev-parse --short HEAD`
+    randomNumber=${(l:5::0:)${RANDOM}}
+    newBranch="$baseBranch-hotfix-albert_$shortCommitHash"_"$randomNumber-GIS-1883"
+
+    echo "Creating a new branch $newBranch and switching into it"
+    git checkout -b $newBranch origin/$currentBranch
+    git push -u origin $newBranch
+}
